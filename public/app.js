937ebc595e9eb83e6300de239e326f4a6ad3da16
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2016-11-06 16:25:06 -0500

    added files

---
+++ public/app.js
@@ -0,0 +1,20 @@
+$.ajax({url:'/article', method:'GET'})
+	.done(function(results){
+		// console.log(results);
+		results.forEach(function(article){
+			var div = $('<div>').addClass('article').data('id', article._id).appendTo('#articles')
+			var title = $('<h2>').addClass('articleTitle').text(article.title).appendTo(div)
+			var link = $('<p>').addClass('articleLink').text(article.link).css('font-style', 'italic').appendTo(div)
+			
+
+		})
+		
+	})
+
+$(document.body).on('click','.article', function(){
+	
+	$('#articleTitleNote').text($(this).children('h2.articleTitle').text())
+	console.log($(this).data('id'));
+
+})
+
---
+++ server.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var cheerio = require('cheerio');
+var mongoose = require('mongoose');
+var request = require('request');
+var bodyParser = require('body-parser')
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+mongoose.connect("mongodb://localhost/week18hw");
+var db = mongoose.connection;
+
+// show any mongoose errors
+db.on('error', function(err) {
+  console.log('Mongoose Error: ', err);
+});
+
+// once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Mongoose connection successful.');
+});
+
+// And we bring in our Note and Article models
+var Note = require('./models/Note.js');
+var Article = require('./models/Article.js');
+
+//scrape articles
+app.get("/scrape", function(req, res){
+
+	//TODO: Pick another site after fuctions are working
+	request('http://www.echojs.com/' ,function(error, response, html){
+		var $ = cheerio.load(html)
+
+		console.log(html);
+
+		$('article h2').each(function(i, element){
+			// save an empty result object
+				var result = {};
+
+				// add the text and href of every link, 
+				// and save them as properties of the result obj
+				result.title = $(this).children('a').text();
+				result.link = $(this).children('a').attr('href');
+
+				console.log(result.title);
+				console.log(result.link);
+
+				var article = new Article(result)
+
+				article.save(function(err, docs){
+					if(err)
+						console.log(err);
+					else
+						console.log(docs);
+				})
+		});
+
+
+	})
+
+	res.send("Scrape Complete");
+})
+
+app.get("/note", function(req, res){
+
+})
+
+//get all articles in database
+app.get("/article", function(req, res){
+	Article.find({}, function(err, docs){
+		if(err)
+			res.send(err)
+		else
+			res.send(docs)
+	})
+})
+
+//push note
+app.post("/new_note", function(req, res){
+	var note = req.body;
+	console.log(note);
+	res.redirect("/")
+})
+
+app.delete("/delete", function(req, res){
+
+})
+
+app.use("/", function(req,res){})
+
+
+app.listen(port, function(){
+	console.log("Im running on port", port);
+})
